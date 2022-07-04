8e4cd71848f410a4108107e94a0af3b5750f066a
Umer Mohammed <umerforlinkedin1442@gmail.com>
2022-07-03 18:24:47 -0700

    100-intel

--- 0x00-hello_world/puts.c
+++ 0x00-hello_world/puts.c
@@ -2,9 +2,6 @@
 /**
  * function_name - Short description, single line
  * @parameterx: Description of parameter x
-(* a blank line
- * Description: Longer description of the function)?
-(* section header: Section description)*
  * Return: Description of the returned value
  */
 int main(void)
