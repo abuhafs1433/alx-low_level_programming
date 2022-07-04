d3ea51339ad4a6e5f15a2abc7db517c532b22acf
Umer Mohammed <umerforlinkedin1442@gmail.com>
2022-07-03 18:40:31 -0700

    101

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -3,11 +3,8 @@
  *main - Entry point
  *Return:Always 0 (Success)
  */
-
 int main(void)
 {
-        puts("and that piece of art is useful - Dora Korpar, 2015-10-19");
-        return(0);
+        puts ("and that piece of art is useful - Dora Korpar, 2015-10-19");
+        return (0);
 }
-
-
