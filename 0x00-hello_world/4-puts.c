d83950ab11d8e9b775d9fcd8ecae4e2a89ba67b9
Umer Mohammed <umerforlinkedin1442@gmail.com>
2022-07-03 17:28:42 -0700

    hello,puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,5 +1,12 @@
 #include <stdio.h>
-/*this is puts function*/
+/**
+ * function_name - Short description, single line
+ * @parameterx: Description of parameter x
+(* a blank line
+ * Description: Longer description of the function)?
+(* section header: Section description)*
+ * Return: Description of the returned value
+ */
 int main(void)
 {
 puts("\"Programming is like building a multilingual puzzle");
