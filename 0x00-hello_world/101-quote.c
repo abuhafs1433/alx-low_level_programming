b38885eb6de2b0bdcd6985d83a44363a507b25a0
Umer Mohammed <umerforlinkedin1442@gmail.com>
2022-07-05 01:44:44 -0700

    101

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,6 +1,5 @@
 #include <stdio.h>
 #include <unistd.h>
-
 /**
  *main - Entry point
  *Return:Always 0 (Success)
@@ -8,5 +7,5 @@
 int main(void)
 {
 write(2, "and that piece of art is useful\" - Dora Korpar, 2016-11-29", 59);
-        return (1);
+return (1);
 }
