a2f9431bf8ac6d820a98b42c5c3ed47a771b432f
Umer Mohammed <umerforlinkedin1442@gmail.com>
2022-07-05 01:42:09 -0700

    101

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,11 +1,12 @@
+#include <stdio.h>
 #include <unistd.h>
+
 /**
  *main - Entry point
  *Return:Always 0 (Success)
  */
 int main(void)
 {
-	/*umer*/
-        write(2, "and that piece of art is useful \" - Dora Korpar, 2016-11-29", 59);
-        return (0);
+write(2, "and that piece of art is useful\" - Dora Korpar, 2016-11-29", 59);
+        return (1);
 }
