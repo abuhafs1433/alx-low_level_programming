e0fb946663661786aa0e3b35cd785a0aa025d4f4
Umer Mohammed <umerforlinkedin1442@gmail.com>
2022-07-06 08:03:06 -0700

    is lower

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -2,9 +2,10 @@
 
 /**
 * _islower -> checks if the character is lowercase
+* @c: a character argument
 * Return: returns 1 and 0 depending on condition
 */
 int _islower(int c)
 {
-return (c >= 'a' && c <= 'z')
+return (c >= 'a' && c <= 'z');
 }
