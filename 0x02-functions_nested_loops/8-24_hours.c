63640433ec9d52c595e50eda50cf1cf678cbfc47
Umer Mohammed <umerforlinkedin1442@gmail.com>
2022-07-06 11:29:33 -0700

    8-24_hours.c

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -1,9 +1,9 @@
 #include "main.h"
-
 /**
- * jack_bauer -> prints 24 hours
- */
-void jack_bauer(void){
+* jack_bauer -> prints 24 hours
+*/
+void jack_bauer(void)
+{
 int i, j;
 
 for (i = 0; i < 24; i++)
@@ -35,4 +35,4 @@ _putchar((j % 10) + '0');
 }
 _putchar('\n');
 }
-}
+
