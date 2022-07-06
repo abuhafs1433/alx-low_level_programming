4e2037534a98886e7643e076f74e880604f32d96
Umer Mohammed <umerforlinkedin1442@gmail.com>
2022-07-06 14:36:06 -0700

    9-times_table.c

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -1,10 +1,14 @@
 #include "main.h"
-/**
-*  times_table -> prints  times_table
-*/
-void times_table(void)
-{
 
+/**
+ * main - check the code
+ *
+ * @s:hjhjhjh
+ * Return: Always 0.
+ */
+void times_table(void)
+int main(void)
+{
 int i, j;
 int mat[SIZE][SIZE];
 for (i = 0; i < SIZE; i++)
@@ -15,6 +19,7 @@ for (i = 0; i < SIZE; i++)
 for (j = 0; j < SIZE; j++)
 print("%5d", mat [i][j]);
 print("\n");
-}
+
 return (0);
+}
 }
