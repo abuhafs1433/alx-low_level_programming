178093469b8cd4b29809e4e30c858e7b5d24b51b
Umer Mohammed <umerforlinkedin1442@gmail.com>
2022-07-06 14:38:45 -0700

    9-times_table.c

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -7,6 +7,7 @@
  * Return: Always 0.
  */
 void times_table(void)
+{
 int main(void)
 {
 int i, j;
@@ -19,7 +20,7 @@ for (i = 0; i < SIZE; i++)
 for (j = 0; j < SIZE; j++)
 print("%5d", mat [i][j]);
 print("\n");
-
+}
 return (0);
 }
 }
