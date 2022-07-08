8089fde675e7e07f55a2198b7c67f43bc2e2ca42
Umer Mohammed <umerforlinkedin1442@gmail.com>
2022-07-08 13:33:44 -0700

    100-prime_factor.c

--- 0x04-more_functions_nested_loops/100-prime_factor.c
+++ 0x04-more_functions_nested_loops/100-prime_factor.c
@@ -2,7 +2,7 @@
 
 /**
 * main - Entry point.
-* 
+*
 * Return: Always 0.
 */
 
@@ -12,13 +12,13 @@ int main(void)
 long n, i;
 
 n = 612852475143;
-for (i = 2; i == 0)
+for (i = 2; i < n; i++)
 {
 while (n % i == 0)
 n = n / i;
 }
 
-printf("%lu\n" , n);
+printf("%lu\n", n);
 
 return (0);
 }
