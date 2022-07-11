519a0ae2705b78b6d3b45855f53affe827f30030
Umer Mohammed <umerforlinkedin1442@gmail.com>
2022-07-11 07:10:22 -0700

    2-strlen.c

--- 0x05-pointers_arrays_strings/2-strlen.c
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -5,7 +5,13 @@
  *  @s: string pointer to passed to this function
  *  Return: returns length of string
  */
-void _strlen(char *s)
+int _strlen(char *s)
 {
+int len;
+
+for (; *s != '\0'; s++)
+{
+len += 1;
+}
 return (strlen(s));
 }
