3dcebf254ea12bc756a5778d6b00be9b205d1cfb
Umer Mohammed <umerforlinkedin1442@gmail.com>
2022-07-14 13:06:19 -0700

    1-strncat.c

---
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * _strncat -> function to append some charx
+ * @dest: first parm
+ * @src: second parm
+ * @n: 3rd param
+ * Return: string
+ */
+char *_strncat(char *dest, char *src, int n)
+{
+strncat(dest, src, n);
+return (dest);
+}
