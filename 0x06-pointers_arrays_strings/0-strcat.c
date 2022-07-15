b94bf743c8a9d835a1ec4c183856953cf4cfcbef
Umer Mohammed <umerforlinkedin1442@gmail.com>
2022-07-15 01:57:12 -0700

    0-strcat.c

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -9,15 +9,9 @@
  */
 char *_strcat(char *dest, char *src)
 {
-int len = 0, i;
-while (dest[len])
-len++;
-for (i = 0; src[i] != '0'; i++)
-{
-dest[len] = src[i];
-len += 1;
-}
-dest[len] = '\0';
+int index = 0, dest_len = 0;
+while (dest[index++])
+dest_len++;
+for (index = 0; src[index]; index++)
 return (dest);
 }
-
