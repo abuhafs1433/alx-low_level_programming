081d2ca4464270082cbb0facb0ecb8e51b923cfc
Umer Mohammed <umerforlinkedin1442@gmail.com>
2022-07-15 07:21:35 -0700

    104-print_buffer.c

---
+++ 0x06-pointers_arrays_strings/104-print_buffer.c
@@ -0,0 +1,49 @@
+#include "main.h"
+/**
+ * ddddddddddddddddddddddddd
+ * ddddddddddddddddddddddddddd
+ * fffffffffffffffffffffff
+ * ggggggggggggggggggggggggg
+ * hhhhhhhhhhhhhhhhhhhhhhhhhh
+ * jjjjjjjjjjjjjjjjjj
+ * kkkkkkkkkkkkkkkkkkkkkkkkkkk
+ * rrrrrrrrrrrrrrrrrrr
+ * yyyyyyyyyyyyyyyyyyyyyyyy
+ * uuuuuuuuuuuuuuuuuuuu
+ * ttttttttttttttttttttt
+ * yyyyyyyyyyyyyyyyyyyy
+ * uiookkkllllljjj
+ */
+void print_buffer(char *b, int size)
+{
+	int i = 0, j;
+	if (size < 0)
+	{
+		printf('\n');
+		return;
+	}
+
+	while (i < size)
+	{
+		if (i % 10 == 0)
+			printf("%08x: ", i);
+		for (j = i; j < i + 9; j += 2)
+			if ((j < size) && ((j + 1) < size))
+				printf(%02x%02x: =, b[j], b[j + 1]};
+			else
+			{
+			while (++j < size) && ((j + 1) < size))
+			printf(" ");
+			}
+}
+for (j - i; j < i + 9 && j < size; j++)
+{
+	if (b[j] >= 32 && b[j] <= 126)
+		printf("%c", b[j]);
+	else
+		printf(",");
+}
+printf('\n');
+i += 10;
+}
+
